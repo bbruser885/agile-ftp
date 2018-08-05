2c7536eef9832ad10c11602844ede1e2ad6576ad
Daniel Reimer <daniel.k.reimer97@gmail.com>
2018-08-04 21:23:44 -0700

    add tests for CmdList

--- Tests/AgileFTPTests.cs
+++ Tests/AgileFTPTests.cs
@@ -7,22 +7,40 @@ namespace AgileFTP.Tests
     public class AgileFTPTests
     {
 	[Fact]
-	public void CmdExit_AlwaysTrueNoArgs()
+	public void CmdExit_TrueWithNoArgs()
 	{
 	    CmdExit _exit = new CmdExit();
 	    string[] args = {""};
 	    var result = _exit.Validate(args);
-	    Assert.True(result, "Exit should always return true");
+	    Assert.True(result, "Exit should return true with no arguments");
 	}
 
 	[Fact]
-	public void CmdExit_AlwaysTrueWithArgs()
+	public void CmdExit_TrueWithMultipleArgs()
 	{
 	    CmdExit _exit = new CmdExit();
 	    string[] args = {"string1", "string2"};
 	    var result = _exit.Validate(args);
-	    Assert.True(result, "Exit should always return true");
+	    Assert.True(result, "Exit should return true with multiple arguments");
 
+	}
+
+	[Fact]
+	public void CmdList_TrueWithNoArgs()
+	{
+	    CmdList _list = new CmdList();
+	    string[] args = {""};
+	    var result = _list.Validate(args);
+	    Assert.True(result, "List should return true with no arguments");
+	}
+
+	[Fact]
+	public void CmdList_TrueWithMultipleArgs()
+	{
+	    CmdList _list = new CmdList();
+	    string[] args = {"string1", "string2"};
+	    var result = _list.Validate(args);
+	    Assert.True(result, "List should return true with multiple arguments");
 	}
     }
 }
