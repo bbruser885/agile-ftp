c5e81883e4b1103b4ad734869e853c8b5909eb55
Daniel Reimer <daniel.k.reimer97@gmail.com>
2018-08-04 21:40:04 -0700

    add tests for CmdLocalList

--- Tests/AgileFTPTests.cs
+++ Tests/AgileFTPTests.cs
@@ -9,7 +9,7 @@ namespace AgileFTP.Tests
 	public void CmdExit_TrueWithNoArgs()
 	{
 	    CmdExit _exit = new CmdExit();
-	    string[] args = {""};
+	    string[] args = {};
 	    var result = _exit.Validate(args);
 	    Assert.True(result, "Exit should return true with no arguments");
 	}
@@ -28,7 +28,7 @@ namespace AgileFTP.Tests
 	public void CmdList_TrueWithNoArgs()
 	{
 	    CmdList _list = new CmdList();
-	    string[] args = {""};
+	    string[] args = {};
 	    var result = _list.Validate(args);
 	    Assert.True(result, "List should return true with no arguments");
 	}
@@ -40,6 +40,24 @@ namespace AgileFTP.Tests
 	    string[] args = {"string1", "string2"};
 	    var result = _list.Validate(args);
 	    Assert.True(result, "List should return true with multiple arguments");
+	}
+
+	[Fact]
+	public void CmdLocalList_FalseWithNoArgs()
+	{
+	    CmdLocalList _list = new CmdLocalList();
+	    string[] args = {};
+	    var result = _list.Validate(args);
+	    Assert.False(result, "Local list should return false with no arguments");
+	}
+
+	[Fact]
+	public void CmdLocalList_FalseWithInvalidFilename()
+	{
+	    CmdLocalList _list = new CmdLocalList();
+	    string[] args = {"this should never be a valid filename"};
+	    var result = _list.Validate(args);
+	    Assert.True(result, "Local list should return false with invalid filename");
 	}
     }
 }
